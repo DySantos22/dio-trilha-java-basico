0b9ed3ab58d4393301c6550516fdc4f9fbe11030
DySantos22 <dyllanlima17@gmail.com>
2023-10-07 01:07:59 -0300

    Projeto realizando um sistema de Processo Seletivo

---
+++ projetos-curso/Candidatura/src/ProcessoSeletivo.java
@@ -0,0 +1,12 @@
+public class ProcessoSeletivo {
+    public static void main(String[] args){
+        System.out.println("Processo Seletivo");
+
+
+    }
+    static void AnalisarCandidato(double salariopretendido){
+        double salariobase = 2000.0;
+
+        String decisao = salariobase > salariopretendido ? "LIGAR PARA CANDIDATO" : "";
+    }
+}
